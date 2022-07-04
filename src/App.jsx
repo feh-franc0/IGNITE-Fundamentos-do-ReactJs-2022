50b37f23c1d08dc1aeef2da1b7e67756b5ca04fb
feh-franc0 <fernandofv1110@gmail.com>
2022-07-04 19:37:34 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <>
-    <Header />
+      <Header />
 
-    <Post 
-      author="Fernando Franco" 
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, provident numquam. Unde dolore quod repudiandae numquam error, similique iste dolorum distinctio culpa in magni quasi sed saepe quidem labore veritatis?" 
-    />  
-    <Post 
-      author="Felipe Marques" 
-      content="texto de ilustracao para ser exibido na tela" 
-    />  
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Fernando Franco" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, provident numquam. Unde dolore quod repudiandae numquam error, similique iste dolorum distinctio culpa in magni quasi sed saepe quidem labore veritatis?" 
+          />  
+          <Post 
+            author="Felipe Marques" 
+            content="texto de ilustracao para ser exibido na tela" 
+          />
+        </main>
+      </div>  
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1635830625698-3b9bd74671ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+            />
+
+            <div className={styles.profile}>
+                <strong>Fernando Franco</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
