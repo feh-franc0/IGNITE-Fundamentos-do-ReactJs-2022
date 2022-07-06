1a6a0fce4317daced03b50f73c5f90bce85ebf0e
feh-franc0 <fernandofv1110@gmail.com>
2022-07-05 21:52:07 -0300

    Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,8 +18,12 @@ export function Post() {
             <div className={styles.content}>
                 <p>Fala galera 👋 </p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare🚀</p>
-                <p> 👉 <a href="#"> jane.design/doctorcare </a> </p>
-                <p> <a href="#"> #novoprojeto #nlw #rocketseat </a> </p>
+                <p><a href="#">jane.design/doctorcare</a></p>
+                <p> 
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketsea</a>
+                </p>
             </div>
         </article>
     )
