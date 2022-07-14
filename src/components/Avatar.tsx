53c88aadd6974b6191bab6d52605872e92240d48
feh-franc0 <fernandofv1110@gmail.com>
2022-07-13 21:22:35 -0300

    TypeScript no React

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,11 +1,18 @@
 import styles from './Avatar.module.css'
 
+interface AvatarProps {
+    hasBorder?: boolean;
+    src: string;
+    alt?: string;
+}
+
 //* usando desestruturação no props (props) = ({hasBorder=true, src})
-export function Avatar({ hasBorder = true, src }) {
+export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
     return (
         <img 
             className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
             src={src} 
+            alt={alt}
         />
     );
 }
